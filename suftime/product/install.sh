4b482152329243399daddafdb9926cf20d55880a
sami2020pro <64816951+sami2020pro@users.noreply.github.com>
2020-11-12 01:11:01 +0330

    Update install.sh

--- suftime/product/install.sh
+++ suftime/product/install.sh
@@ -1,6 +1,6 @@
 sudo chmod +x suftime 
-sudo mkdir /etc/suftime 
+sudo mkdir /etc/suftime
 sudo cp help.txt /etc/suftime/
 sudo cp version.txt /etc/suftime/
-sudo cp suftime /bin
+sudo cp suftime /usr/local/bin
 echo "Suftime Installed"
